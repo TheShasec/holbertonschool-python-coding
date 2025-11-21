650d81781a2f601f1cc58813e1bfa60310018b94
theshasec <theshasec@gmail.com>
2025-11-21 14:05:23 +0000

    4324

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""dfsdf"""
+
+
+class Square:
+    """dfsdfsd"""
+    def __init__(self, size=0):
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        self.__size = size
